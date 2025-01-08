6866da722fc563a06a9ce80718cca20cf49009ed
Sanjay Gupta <166055466+Sanjaycodes019@users.noreply.github.com>
2025-01-08 15:05:12 +0000

    commit

--- pattern/02.cpp
+++ pattern/02.cpp
@@ -3,9 +3,10 @@ using namespace std;
 int main(){
     int n = 5;
     for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
-            cout <<"*";
+        for (int j = 0; j <= i; j++){
+            cout<<"*";
         }
-        cout <<endl;
+        cout<<endl;
     }
 }
+
--- pattern/04.cpp
+++ pattern/04.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 int main(){
     int n = 5;
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
-            cout <<"*";
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= i; j++){
+            cout << i <<" ";
         }
         cout <<endl;
     }
