59761cdac20fa6d2240d7f5b3ab867c94085472c
Sanjay Gupta <166055466+Sanjaycodes019@users.noreply.github.com>
2025-01-10 07:55:33 +0000

    commit

--- hashing/01.cpp
+++ hashing/01.cpp
@@ -5,19 +5,7 @@ int main() {
     int n = 6;
     int arr[] = {1, 2, 2, 3, 3, 3};
 
-    // Precompute
-    int hash[13] = {0};  // Adjust size if needed
-    for (int i = 0; i < n; i++) {
-        hash[arr[i]]++;
-    }
-
-    int q = 3;
-    int queries[] = {1, 2, 3};
-
-    // Fetch and output results for pre-defined queries
-    for (int i = 0; i < q; i++) {
-        cout << hash[queries[i]] << endl;
-    }
+    
 
     return 0;
 }
