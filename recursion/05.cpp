588b72e1a079b5b8859baf7f32c30743660262fc
Sanjay Gupta <166055466+Sanjaycodes019@users.noreply.github.com>
2025-01-10 03:30:18 +0000

    commit

--- recursion/05.cpp
+++ recursion/05.cpp
@@ -1,1 +1,22 @@
-// reverse of an array using recursion
+#include <bits/stdc++.h>
+using namespace std;
+
+void f(int i, int arr[], int n) {
+    if (i >= n / 2) return;
+    swap(arr[i], arr[n - i - 1]);
+    f(i + 1, arr, n);
+}
+
+int main() {
+    int arr[] = {1, 2, 4, 5, 6, 7, 65, 3, 45, 54};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    f(0, arr, n);
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
