f2cb550fc34f9b24f42fa148a2212a15350c9507
Sanjay Gupta <166055466+Sanjaycodes019@users.noreply.github.com>
2025-01-22 12:19:00 +0000

    commit

---
+++ stack/01.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Stack {
+    int size;
+    int top;
+    int *arr;
+
+public:
+    Stack(int size) {
+        this->size = size;
+        top = -1;
+        arr = new int[size];
+    }
+
+    // Push an element onto the stack
+    void push(int value) {
+        if(top == size - 1) {
+            cout << "Stack Overflow!" << endl;
+            return;
+        }
+        arr[++top] = value;
+    }
+
+    // Pop an element from the stack
+    int pop() {
+        if(top == -1) {
+            cout << "Stack Underflow!" << endl;
+            return -1;
+        }
+        return arr[top--];
+    }
+
+    // Return the current size of the stack
+    int currentSize() {
+        return top + 1;
+    }
+
+    // Return the element at the top of the stack
+    int getTop() {
+        if(top == -1) {
+            cout << "Stack is empty!" << endl;
+            return -1;
+        }
+        return arr[top];
+    }
+
+    // Check if the stack is empty
+    bool isEmpty() {
+        return top == -1;
+    }
+};
+
+int main() {
+    Stack s(5);
+    
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    s.push(45);
+    s.push(56);
+    
+    cout << "Top element: " << s.getTop() << endl;
+
+    while(!s.isEmpty()) {
+        cout << s.pop() << endl; // this method traverses the stack array
+    }
+    
+    return 0;
+}
