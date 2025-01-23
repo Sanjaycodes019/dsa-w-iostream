686b3d2c7eb9127bcd4145a2da624541f6bc1963
Sanjay Gupta <166055466+Sanjaycodes019@users.noreply.github.com>
2025-01-23 11:39:21 +0000

    commit

--- stack/03.cpp
+++ stack/03.cpp
@@ -1,9 +1,31 @@
-//reverse an array
-#include<bits/stdc++.h>
+// reverse Array
+#include <bits/stdc++.h>
 using namespace std;
 
+int main() {
+    char arr[] = {'a', 'b', 'c', 'd', 'e'};
+    int n = sizeof(arr) / sizeof(arr[0]); 
 
+    stack<char> s;
 
-int main(){
+    for (int i = 0; i < n; i++) {
+        s.push(arr[i]);
+    }
+
+    int i = 0;
+    while (!s.empty()) {
+        arr[i] = s.top();
+        s.pop();
+        i++;
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
     return 0;
 }
+
+// timecomplexity = O(n)
+// spaceComplexity = O(n)
