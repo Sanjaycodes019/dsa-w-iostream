350495b2b4c4648fe17f211e497b72ea8051c934
Sanjay Gupta <166055466+Sanjaycodes019@users.noreply.github.com>
2025-02-10 04:26:49 +0000

    commit

---
+++ linkedlist/04.cpp
@@ -0,0 +1,45 @@
+//middle of linkedlist
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+// Function to find the middle of the linked list
+Node* findMiddle(Node* head) {
+    if (head == NULL) return NULL;
+
+    Node* slow = head;
+    Node* fast = head;
+
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;  // Middle node
+}
+
+// Driver Code
+int main() {
+    // Creating a linked list manually
+    Node* head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+    head->next->next->next->next = new Node(5);
+
+    Node* middle = findMiddle(head);
+    if (middle != NULL)
+        cout << "Middle Element: " << middle->data << endl;
+    else
+        cout << "List is empty." << endl;
+
+    return 0;
+}
