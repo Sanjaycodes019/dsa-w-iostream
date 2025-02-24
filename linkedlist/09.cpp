74f9b4c36a998f2f2652bbf418fb644c73d5b075
Sanjay Gupta <166055466+Sanjaycodes019@users.noreply.github.com>
2025-02-24 15:47:02 +0000

    commit

--- linkedlist/09.cpp
+++ linkedlist/09.cpp
@@ -1,1 +1,63 @@
 // removing head
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+    Node(int val) : data(val), next(nullptr) {}
+};
+
+class LinkedList {
+public:
+    Node* head;
+    LinkedList() : head(nullptr) {}
+
+    void insert(int val) {
+        Node* newNode = new Node(val);
+        if (!head) {
+            head = newNode;
+            return;
+        }
+        Node* temp = head;
+        while (temp->next) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+
+    void display() {
+        Node* temp = head;
+        while (temp) {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+
+    void removeHead() {
+        if (!head) return;
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+    }
+};
+
+int main() {
+    LinkedList list;
+    vector<int> arr = {1, 3, 4, 2, 5};
+    for (int val : arr) {
+        list.insert(val);
+    }
+
+    cout << "Original List: ";
+    list.display();
+
+    list.removeHead();
+
+    cout << "After Removing Head: ";
+    list.display();
+
+    return 0;
+}
