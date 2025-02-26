fbea3d7ae0dfe538a5294af7c124777c303e8453
Sanjay Gupta <166055466+Sanjaycodes019@users.noreply.github.com>
2025-02-26 16:21:13 +0000

    commit

--- array/21ds.cpp
+++ array/21ds.cpp
@@ -1,1 +1,36 @@
 // intersection, two array
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> intersectionOfArrays(vector<int>& a, vector<int>& b) {
+    int i = 0, j = 0;
+    vector<int> intersectionArr;
+
+    while (i < a.size() && j < b.size()) {
+        if (a[i] == b[j]) {
+            if (intersectionArr.empty() || intersectionArr.back() != a[i]) 
+                intersectionArr.push_back(a[i]);
+            i++, j++;
+        } 
+        else if (a[i] < b[j]) 
+            i++;
+        else 
+            j++;
+    }
+    
+    return intersectionArr;
+}
+
+int main() {
+    vector<int> a = {1, 2, 4, 5, 6};
+    vector<int> b = {2, 3, 5, 7};
+
+    vector<int> result = intersectionOfArrays(a, b);
+
+    cout << "Intersection: ";
+    for (int num : result) 
+        cout << num << " ";
+
+    return 0;
+}
