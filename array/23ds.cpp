fca843a8b6d92443ebe79cd34bb9d74f55638950
Sanjay Gupta <166055466+Sanjaycodes019@users.noreply.github.com>
2025-03-03 13:11:26 +0000

    commit

--- array/23ds.cpp
+++ array/23ds.cpp
@@ -1,1 +1,23 @@
 // Max Consecutive number of 1's
+#include <bits/stdc++.h>
+using namespace std;
+int findMaxConsecutiveOnes(int* nums, int numsSize) {
+    int max1 = 0;
+    int count = 0;
+    
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] == 1) {
+            count++;
+            max1 = max(max1, count);
+        } else {
+            count = 0;
+        }
+    }
+    return max1;
+}
+int main() {
+    int nums[] = {1, 1, 0, 1, 1, 1};
+    int numsSize = sizeof(nums) / sizeof(nums[0]);
+    cout << "Max Consecutive Ones: " << findMaxConsecutiveOnes(nums, numsSize) << endl;
+    return 0;
+}
