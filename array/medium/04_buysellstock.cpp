ae1d556826c328cbea5caa0b20c0439639fa6d51
Sanjay Gupta <166055466+Sanjaycodes019@users.noreply.github.com>
2025-03-24 12:44:57 +0000

    commit

---
+++ array/medium/04_buysellstock.cpp
@@ -0,0 +1,32 @@
+// 💰 Leetcode 121 - Best Time to Buy and Sell Stock
+// Given an array 'prices' where prices[i] is the price of a stock on the ith day,
+// find the maximum profit you can achieve from one transaction (buy one and sell one share of the stock).
+// If you cannot achieve any profit, return 0.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxProfit(vector<int>& prices) {
+    int mini = prices[0];
+    int maxProfit = 0;
+    int n = prices.size();
+
+    for(int i = 0; i < n; i++) {
+        int cost = prices[i] - mini;
+        maxProfit = max(maxProfit, cost);
+        mini = min(mini, prices[i]);
+    }
+    return maxProfit;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> prices(n);
+    for(int i = 0; i < n; i++) {
+        cin >> prices[i];
+    }
+
+    cout << maxProfit(prices) << endl;
+    return 0;
+}
