647a48374247ee9f6d2ce14a03a5461875b21160
Sanjay Gupta <166055466+Sanjaycodes019@users.noreply.github.com>
2025-04-23 17:40:16 +0000

    commit

--- binarysearchtree/01.cpp
+++ binarysearchtree/01.cpp
@@ -5,54 +5,59 @@
 // search, add, or remove numbers quickly
 // faster than a list
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Node{
-    public:
+class Node {
+public:
     int data;
     Node* left;
     Node* right;
 
-    Node(int val){
-        this->data = val;
-        this->left = NULL;
-        this->right = NULL;
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
     }
 };
 
-    Node* insertIntoBST(Node* &root, int val){
-        if(root==NULL){
-            root = new Node(val);
-            return root;
-        }
-        if(val>root->data){
-            root->right = insertIntoBST(root->right, val);
-        }
-        else{
-            root->left = insertIntoBST(root->left, val);
-        }
-        return root;
+// insert function (outside the Node class)
+Node* insertIntoBST(Node* root, int val) {
+    if (root == NULL) {
+        return new Node(val);
     }
+    if (val > root->data) {
+        root->right = insertIntoBST(root->right, val);
+    } else {
+        root->left = insertIntoBST(root->left, val);
+    }
+    return root;
+}
 
-    void takeInput(Node* root){
-        int data;
-        cin>>data;
-        while(data!=-1){
-            insertIntoBST(root, data);
-            cin>>data;
-        }
-    }
-    void inOrder(Node* root) {
-        if (root == NULL) return;
-        inOrder(root->left);
-        cout << root->data << " ";
-        inOrder(root->right);
-    }
-
-int main(){
+// function to take input from user
+Node* takeInput() {
+    int data;
     Node* root = NULL;
-    takeInput(root);
+    cin >> data;
+    while (data != -1) {
+        root = insertIntoBST(root, data);
+        cin >> data;
+    }
+    return root;
+}
+
+// in-order traversal to print BST
+void inOrder(Node* root) {
+    if (root == NULL) return;
+    inOrder(root->left);
+    cout << root->data << " ";
+    inOrder(root->right);
+}
+
+int main() {
+    cout << "Enter values to insert in BST (enter -1 to stop): ";
+    Node* root = takeInput();
+    cout << "Inorder Traversal of BST: ";
     inOrder(root);
     return 0;
 }
